0f9cb3e072b0ccea92f398214f6255ba9c0fd010
Peter Viegas <peterviegas@outlook.com>
2022-12-15 12:24:06 +0000

    re-factored transaction processor

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,52 +1,16 @@
-var txr = [];
+function processTransactions(transactions) {
 
-function processTransactions(transActions) {
-
-    txr = [];
-
-    if(!validateTransactions(transActions)) {
+    if (!transactions) {
         throw new Error("Undefined collection of transactions")
     }
 
-    let txCount = {}
+    const txCount = transactions.reduce((txact, item) => { return txact[item] ? ++txact[item] : txact[item] = 1, txact }, {});
 
-    const numberOfTransactions = transActions.length;
-
-    for(var i = 0; i < numberOfTransactions; i++) {
-        const transaction = transActions[i];
-        txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
-    }
-
-    txCount = sortByAmountThenName(txCount);
-    
-    // Place them back in array for returning
-    Object.keys(txCount).forEach(function (key, index) {
-        txr[index] = `${key} ${txCount[key]}`;
+    const sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
+        return txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)
     });
 
-    return txr;
-}
-
-function sortByAmountThenName(txCount) {
-    let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
-        return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
-    );
-
-    let sortedResults = {};
-    for(let objectKey of sortedKeys) {
-        sortedResults[objectKey] = txCount[objectKey];
-    }
-
-    return sortedResults;
-}
-
-
-function validateTransactions(transactions) {
-    if(transactions === undefined) {
-        return false;
-    } 
-
-    return true;
+    return sortedKeys.map((key) => `${key} ${txCount[key]}`);
 }
 
 module.exports = processTransactions;
